7bdf3296f6d49920b8a5f517bcca63ee1e5a7a19
juancrafting18 <65603518+juancrafting18@users.noreply.github.com>
2023-03-07 18:20:21 -0500

    Add files via upload

---
+++ dayOfWomen/main.js
@@ -0,0 +1,5 @@
+const imgChayanne = document.querySelector('.img-main-sp');
+imgChayanne.addEventListener('click', dontTouchAlert);
+function dontTouchAlert(){
+    alert('No toques a Chayanne.');
+}
